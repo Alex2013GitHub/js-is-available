c0e35ee95171842a4495e152310c1fda1a1cbfac
Alex2013GitHub <96023225+Alex2013GitHub@users.noreply.github.com>
2022-05-29 19:51:41 +0200

    js-1

---
+++ src/src/js/task-01.js
@@ -0,0 +1,8 @@
+const counCategori = document.querySelectorAll(".item");
+console.log(`Number of categories: ${counCategori.length}`);
+
+for(const item of counCategori){
+    console.log(`Category: ${item.firstElementChild.textContent} `);
+    const countItems = item.querySelectorAll("li");
+    console.log(`Elements: ${countItems.length}`);
+}
