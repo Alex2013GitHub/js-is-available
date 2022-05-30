3c52ae2208ae41c514878f19723f8b91e8b0992b
Alex2013GitHub <96023225+Alex2013GitHub@users.noreply.github.com>
2022-05-30 21:07:01 +0200

    02-js

--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+for (let ingredient of ingredients) {
+  const ingredientsEl = document.createElement('li');
+  ingredientsEl.classList.add('item');
+  ingredientsEl.textContent = [ingredient];
+  console.log(ingredientsEl);
+
+  const ulEl = document.querySelector('#ingredients');
+
+  ulEl.appendChild(ingredientsEl);
+}
