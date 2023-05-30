d3cfa0d3a9a99e4c427617456fb44fecfc0eee05
Oleksandr Lapin <96023225+Alex2013GitHub@users.noreply.github.com>
2023-05-30 22:43:22 +0200

    task_available

--- src/src/js/task-05.js
+++ src/src/js/task-05.js
@@ -3,33 +3,29 @@
 // <input type="text" id="name-input" placeholder="Please enter your name" />
 // <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
 
-// const input = document.querySelector("#name-input");
-// const output = document.querySelector("#name-input");
+const input = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
 
 // console.log(input);
 // console.log(output);
 
+// textInputRef.addEventListener('input', onChangeTextInput);
 
-textInputRef.addEventListener('input', onChangeTextInput);
+// function onChangeTextInput({ currentTarget: { value } }) {
+//   textOutputRef.textContent = !value ? 'Anonymous' : value;
+// }
 
-function onChangeTextInput({ currentTarget: { value }}) {
-    textOutputRef.textContent = !value
-        ? 'Anonymous'
-        : value;
-}
-
-// input.addEventListener("input", (event) => {
-//     if(input.textContent.length === "") {
-//         output.textContent = 'Anonymous'
-//     }
-//     outputt.textContent = event.currentTarget.value
-// })
+input.addEventListener('input', event => {
+  if (input.textContent.length === '') {
+    output.textContent = 'Anonymous';
+  }
+  output.textContent = event.currentTarget.value;
+});
 
 // outputRef.textContent =
-//     event.currentTarget.value === "" ? "Anonymous" : event.currentTarget.value;
+//   event.currentTarget.value === '' ? 'Anonymous' : event.currentTarget.value;
 
-    // input.addEventListener('input', (event) => {
-    //     outputRef.textContent =
-    //         event.currentTarget.value === "" ? "Anonymous" : event.currentTarget.value;
-        
-    // })
+// input.addEventListener('input', event => {
+//   outputRef.textContent =
+//     event.currentTarget.value === '' ? 'Anonymous' : event.currentTarget.value;
+// });
