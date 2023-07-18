60ec20264ac70e2ed778232bcbd83461e83659c2
Oleksandr Lapin <96023225+Alex2013GitHub@users.noreply.github.com>
2023-07-18 22:12:39 +0200

    _07_01_js

--- src/src/js/07_01.js
+++ src/src/js/07_01.js
@@ -1,44 +1,95 @@
-const colorPalette = document.querySelector('.color-palette');
-const output = document.querySelector('.output');
+// const colorPalette = document.querySelector('.color-palette');
+// const output = document.querySelector('.output');
 
-colorPalette.addEventListener('click', selectColor);
+// colorPalette.addEventListener('click', selectColor);
 
-// This is where delegation «magic» happens
+// // This is where delegation «magic» happens
 
-function selectColor(event) {
-  if (event.target.nodeName !== 'BUTTON') {
-    return;
-  }
+// function selectColor(event) {
+//   if (event.target.nodeName !== 'BUTTON') {
+//     return;
+//   }
 
-  const selectColor = event.target.dataset.color;
-  output.textContent = `Selected color: - ${selectColor}`;
-  output.style.color = selectColor;
-}
+//   const selectColor = event.target.dataset.color;
+//   output.textContent = `Selected color: - ${selectColor}`;
+//   output.style.color = selectColor;
+// }
 
 // Some helper functions to render palette items
 
-createPaletteItems();
+// createPaletteItems();
 
-function createPaletteItems() {
-  const items = [];
-  for (let i = 0; i < 60; i++) {
-    const color = getRandomColor();
-    const item = document.createElement('button');
-    item.type = 'button';
-    item.dataset.color = color;
-    item.style.backgroundColor = color;
-    item.classList.add('item');
-    items.push(item);
+// function createPaletteItems() {
+//   const items = [];
+//   for (let i = 0; i < 60; i++) {
+//     const color = getRandomColor();
+//     const item = document.createElement('button');
+//     item.type = 'button';
+//     item.dataset.color = color;
+//     item.style.backgroundColor = color;
+//     item.classList.add('item');
+//     items.push(item);
+//   }
+//   colorPalette.append(...items);
+// }
+
+// function getRandomColor() {
+//   return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
+// }
+
+// function getRandomHex() {
+//   return Math.round(Math.random() * 256)
+//     .toString(16)
+//     .padStart(2, '0');
+// }
+
+// <!-- 2 ===================== -->
+
+// 1. отримати всі рефси(фщрма, спан, тотал)
+// 2. повісити слухач подію по шнпуту на всю форму
+// 3. реалізувати ренж
+// 3.1.отримати value ренжу і записати його у спан
+// 4. створимо обєкт у якому збережемо ціну і кількість
+
+const refs = {
+  form: document.getElementById('form'),
+  amount: document.getElementById('amount'),
+  total: document.getElementById('total'),
+};
+
+const data = {
+  price: 0,
+  amount: 0,
+  calcTotalPrice() {
+    return (this.price * this.amount).toFixed(2);
+  },
+};
+
+fillData();
+setAmount();
+displayTotal();
+
+refs.form.addEventListener('input', onInput);
+
+function onInput({ target }) {
+  if (target.name === 'quantity') {
+    setAmount();
   }
-  colorPalette.append(...items);
+  fillData();
+  displayTotal();
 }
 
-function getRandomColor() {
-  return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
+function displayTotal() {
+  refs.total.textContent = `${data.calcTotalPrice()}грн;`;
 }
 
-function getRandomHex() {
-  return Math.round(Math.random() * 256)
-    .toString(16)
-    .padStart(2, '0');
+function fillData() {
+  data.price = refs.form.elements.price.value;
+  data.amount = refs.form.elements.quantity.value;
 }
+
+function setAmount() {
+  refs.amount.textContent = refs.form.elements.quantity.value;
+}
+
+//  3 ==============
